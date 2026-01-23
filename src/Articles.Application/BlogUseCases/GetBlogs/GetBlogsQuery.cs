35ed973d56224ca602b46118e6a6204a073fb628
gritsruslan <gritsruslan@gmail.com>
2026-01-23 15:58:20 +0200

    add PagedData abstraction

--- src/Articles.Application/BlogUseCases/GetBlogs/GetBlogsQuery.cs
+++ src/Articles.Application/BlogUseCases/GetBlogs/GetBlogsQuery.cs
@@ -1,5 +1,6 @@
 using Articles.Domain.ReadModels;
+using Articles.Shared.Abstraction;
 
 namespace Articles.Application.BlogUseCases.GetBlogs;
 
-public sealed record GetBlogsQuery(int Page, int PageSize) : IQuery<IEnumerable<BlogReadModel>>;
+public sealed record GetBlogsQuery(int Page, int PageSize) : IQuery<PagedData<BlogReadModel>>;
