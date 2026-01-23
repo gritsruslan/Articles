79404a92eda688b18a356535e919a6eb45ee103e
gritsruslan <gritsruslan@gmail.com>
2026-01-23 13:28:08 +0200

    article models

---
+++ src/Articles.Storage.Postgres/Entities/ArticleEntity.cs
@@ -0,0 +1,16 @@
+using Articles.Shared.Abstraction;
+
+namespace Articles.Storage.Postgres.Entities;
+
+public sealed class ArticleEntity : BaseAuditableEntity
+{
+	public Guid Id { get; set; }
+
+	public string Title { get; set; } = null!;
+
+	public string Data { get; set; } = null!;
+
+	public Guid CreatorId { get; set; }
+
+	public UserEntity Creator { get; set; } = null!;
+}
